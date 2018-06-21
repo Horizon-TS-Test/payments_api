480f315024a54425a04b62b077a123888bb43831
pattyAllauca <pallauca@horizon-ts.com>
2018-06-21 15:55:41 -0500

    organización de los métodos POST y GET

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-
-  post "/pagar", to:"payments#create"
-  post "/payments/cards", to:"payments#process_card"  # ESTA EN LA VIEW _card_data.haml
-
   get "/checkout", to: "payments#checkout"
-
   get "/carrito", to: "shopping_carts#show"
-
-
+  post "/pagar", to:"payments#create"
+  post "/payments/cards", to:"payments#process_card"  
 end
