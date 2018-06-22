aef3280df4c54498e0a5273bfc6fd2e90d8e0fb9
pattyAllauca <pallauca@horizon-ts.com>
2018-06-22 11:18:17 -0500

    Archivos modificados

--- test/system/productnames_test.rb
+++ test/system/productnames_test.rb
@@ -3,7 +3,6 @@ require "application_system_test_case"
 class ProductnamesTest < ApplicationSystemTestCase
   # test "visiting the index" do
   #   visit productnames_url
-  #
   #   assert_selector "h1", text: "Productname"
   # end
 end
