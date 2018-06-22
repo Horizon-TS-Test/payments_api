e520dfe5dd773dee59bfca4dec6bdd9438f7b3a6
pattyAllauca <pallauca@horizon-ts.com>
2018-06-22 11:16:35 -0500

    Cambios en initializers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "/checkout", to: "payments#checkout"
   get "/carrito", to: "shopping_carts#show"
-  post "/pagar", to:"payments#create"
-  post "/payments/cards", to:"payments#process_card"  
+  post "/pagar", to: "payments#create"
+  post "/payments/cards", to: "payments#process_card"  
 end
