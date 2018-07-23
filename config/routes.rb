bcdfd5d65d1d80e2a62be1022c2066f0430cac05
pattyAllauca <pallauca@horizon-ts.com>
2018-07-23 17:08:06 -0500

    modificacion nombre ruta

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get "/checkout", to: "payments#checkout"
   get "/carrito", to: "shopping_carts#show"
   post "/pagar", to: "payments#create"
-  post "/payments/cards", to: "payments#process_card"  
+  post "/card", to: "payments#process_card"  
 end
